035ad08dc3c0f6f851b010f0cda9b88aad8ad5e3
Chino Franco <chino.franco@gmail.com>
2023-01-05 16:25:13 +0900

    Added Chapter 1 exercises

---
+++ Chapter 1: Introduction/C1-exercises/exercise_c1.py
@@ -0,0 +1,23 @@
+"""
+CHAPTER 1 - Exercise 1
+
+Let's get started with some basic exercise routines. This exercise will
+get you started on running Python programs and working with variables
+"""
+
+# Replace the 'None' with a string with your name
+name = None
+
+print(f'Hi {name}! Welcome to Python!')
+
+# Fix the code below to make the equation work.
+x = 5
+y = "6"
+
+# Don't touch this part, just used to check your answer
+try:
+    # If the answer is correct, this will be run
+    result = x + y
+    print(f'If we add 5 and 6, we get 11!')
+except:
+    print("Uh oh, looks like you put something wrong here...")
---
+++ Chapter 1: Introduction/C1-exercises/exercise_c2.py
@@ -0,0 +1,41 @@
+"""
+Chapter 1 - Exercise 2
+
+Let's step it up a notch! This exercise will combine the topics you have 
+learned and make you use the topics together.
+
+Write a Python program that does the following:
+- Defines a variable 'x' that is equal to 5.
+- Defines a variable 'y' that is equal to 10.
+- Defines a variable 'z' that is equal to x + y.
+- Defines a function 'add()' that takes two arguments and returns the sum of the arguments.
+- Defines a function 'power()' that takes two arguments and returns the result of raising the first argument to the power of the second argument.
+
+Notes:
+- You may use any built-in Python functions and methods that are relevant to the assignment.
+- You must not use any external libraries or modules in your solution.
+- Your solution must be well-documented and include appropriate comments.
+"""
+
+# Define the variables, replace 'None' with appropriate values
+x = None
+y = None
+z = None
+
+# Check the values of the variables
+print(f'x = {x}, y = {y}, z = {z}')
+
+def add(num1, num2):
+    # Put your code here, replace the '...'
+    ...
+    
+def power(num1, num2):
+    # Put your code here, replace the '...'
+    ...
+    
+# Check the output of the functions
+sample_num1 = 2
+sample_num2 = 3
+print(f'num1 = {sample_num1}, num2 = {sample_num2}')
+print(f'Output of add function = {add(sample_num1, sample_num2)}')
+print(f'Output of power function = {power(sample_num1, sample_num2)}')
